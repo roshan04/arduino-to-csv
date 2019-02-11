a16c9027c36996b9fbb00bc756cc12cb3dc65c6b
ROSHAN KISHOR NIKAM <47280057+roshan04@users.noreply.github.com>
2019-02-11 16:20:14 +0530

    Add files via upload

---
+++ roshan.py
@@ -0,0 +1,15 @@
+import serial
+import csv
+serial_port="/dev/ttyACM0"
+baud_rate=9600
+write_to_file_path="out.xml"
+#output_file=open(write_to_file_path,"w+")
+file2_name=open("anku.csv","w+")
+csv_writer=csv.writer(file2_name)
+ser=serial.Serial(serial_port,baud_rate)
+while True:
+    line=ser.readline();
+    line = line.decode("utf-8")  # ser.readline returns a binary, convert to string
+    # print(line);
+    csv_writer.writerow(line)
+
