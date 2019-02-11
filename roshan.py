bcb6c0977e01561f29cfb4ed11cbf17b191ee1e9
ROSHAN KISHOR NIKAM <47280057+roshan04@users.noreply.github.com>
2019-02-11 16:20:47 +0530

    Update roshan.py

--- roshan.py
+++ roshan.py
@@ -3,7 +3,6 @@ import csv
 serial_port="/dev/ttyACM0"
 baud_rate=9600
 write_to_file_path="out.xml"
-#output_file=open(write_to_file_path,"w+")
 file2_name=open("anku.csv","w+")
 csv_writer=csv.writer(file2_name)
 ser=serial.Serial(serial_port,baud_rate)
